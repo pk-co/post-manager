4b88f1d4712db82121b649adc9061caab7ba25ae
Prasa K.j <118862409+pk-co@users.noreply.github.com>
2023-10-28 08:41:19 +0530

    initial commit: Set up Express server

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log('hello world');
+const  express = require('express');
+const app = express();
+require('dotenv').config();
+
+
+app.get('/', (req,res)=>{
+    res.send('Hello API');
+})
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, ()=>{
+    console.log(`server is listning ${port}`);
+})
